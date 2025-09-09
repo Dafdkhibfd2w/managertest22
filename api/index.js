5e86ee9c6c784e38e4e769e79d6d8796b6b38536
aviel <avielyosefov09@gmail.com>
2025-09-09 10:12:59 +0300

    First commit of manager project

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const app = require('../server');
 
 module.exports = (req, res) => {
-  // מסיר את /api מהנתיב כדי שראוטים כמו "/invoices" יתאימו
+  // מסיר /api מתחילת הנתיב, כדי שהראוטים שלך /save-shift, /orders וכו' יתאימו
   if (req.url.startsWith('/api')) {
     req.url = req.url.replace(/^\/api/, '');
   }
